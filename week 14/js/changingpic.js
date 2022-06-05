28a654c4f422ac04e4911679681466d4a94be649
annaeliava <annaeliavad@gmail.com>
2022-06-05 16:45:02 +0300

    changingpic

---
+++ week 14/js/changingpic.js
@@ -0,0 +1,20 @@
+function changeMe() {
+    let livroomImg = document.getElementById('livingRoom');
+    livroomImg.src = "/week 14/img/changingpic/living-room2.jpg";
+}
+
+function changeMe2() {
+    let kitchenImg = document.getElementById('kitchen');
+    kitchenImg.src = "/week 14/img/changingpic/kitchen2.jpg";
+}
+
+function changeMe3() {
+    let bedroomImg = document.getElementById('bedroom');
+    bedroomImg.src = "/week 14/img/changingpic/bedroom2.jpg";
+}
+
+function changeMe4() {
+    let bathroomImg = document.getElementById('bathroom');
+    bathroomImg.src = "/week 14/img/changingpic/bathroom2.jpg";
+}
+
