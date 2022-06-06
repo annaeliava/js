c9551f426aab91bff29b08a27283fd6d64180843
annaeliava <annaeliavad@gmail.com>
2022-06-06 03:40:07 +0300

    простейший калькулятор. выдает ответ alert-ом

---
+++ week 13/js/calculator2.0.js
@@ -0,0 +1,30 @@
+let numberLeft;
+let numberRight;
+let operator;
+
+const sum = () => {
+    let numberLeft = document.getElementById('left');
+    let numberRight = document.getElementById('right');
+    alert(Number(numberLeft.value)+Number(numberRight.value));
+}
+
+const minus = () => {
+    let numberLeft = document.getElementById('left');
+    let numberRight = document.getElementById('right');
+    alert(Number(numberLeft.value)-Number(numberRight.value));
+}
+
+const multiply = () => {
+    let numberLeft = document.getElementById('left');
+    let numberRight = document.getElementById('right');
+    alert(Number(numberLeft.value)*Number(numberRight.value));
+}
+const divide = () => {
+    let numberLeft = document.getElementById('left');
+    let numberRight = document.getElementById('right');
+    alert(Number(numberLeft.value)/Number(numberRight.value));
+}
+
+
+
+
