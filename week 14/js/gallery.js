e4e17c843d57c66b8a286c330787bf1c82575eda
annaeliava <annaeliavad@gmail.com>
2022-06-19 01:58:03 +0300

    кнопка назад плохо проработана

--- week 14/js/gallery.js
+++ week 14/js/gallery.js
@@ -1,9 +1,29 @@
-let currentImage;
+var i = 0;
+var images = [];
 
-function prev() {
+images[0] = '/week 14/img/gallery/gallery1.jpg';
+images[1] = '/week 14/img/gallery/gallery2.jpg';
+images[2] = '/week 14/img/gallery/gallery3.jpg';
+images[3] = '/week 14/img/gallery/gallery4.jpg';
+images[4] = '/week 14/img/gallery/gallery5.jpg';
+images[5] = '/week 14/img/gallery/gallery6.jpg'
 
-}
+
+function prev() { 
+    document.getElementById('currentImg').src = images[i];
+    if (i < images.length) {
+        i++;
+    } else {
+        i=0;
+    }
+} 
 
 function next() {
-    
+    document.getElementById('currentImg').src = images[i];
+    if (i < images.length - 1) {
+        i++;
+    }else {
+        i=0;
+    }
 }
+
