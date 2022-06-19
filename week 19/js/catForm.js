e62d30317bf79db62aa351f183ba3cea5c76bfd2
annaeliava <annaeliavad@gmail.com>
2022-06-20 00:27:21 +0300

    помогите с этим, у меня и в коносль ничего нет

---
+++ week 19/js/catForm.js
@@ -0,0 +1,25 @@
+/*class Cat {
+    constructor(name, owner){
+        this.name = name;
+        this.owner = owner;
+    }
+}
+
+let cat1 = new Cat('Блонда', 'Софья');
+console.log(cat1.name);*/
+
+/*class Cat {
+    constructor(name, owner, phone, breed){
+        this.name = name;
+        this.owner = owner;
+        this.phone = phone;
+        this.breed = breed;
+    }
+}
+
+let cat = new Cat('Sunny', 'Anna', 789456123, 'british shorthair');
+console.log(cat.name);
+console.log(cat.breed);*/
+
+
+
