d731c4e898ee508209c7b54e473dc4ae0b66706a
annaeliava <annaeliavad@gmail.com>
2022-07-23 19:46:29 +0300

    +

--- week 19/js/catForm.js
+++ week 19/js/catForm.js
@@ -1,14 +1,4 @@
-/*class Cat {
-    constructor(name, owner){
-        this.name = name;
-        this.owner = owner;
-    }
-}
-
-let cat1 = new Cat('Блонда', 'Софья');
-console.log(cat1.name);*/
-
-/*class Cat {
+class Cat {
     constructor(name, owner, phone, breed){
         this.name = name;
         this.owner = owner;
@@ -17,9 +7,9 @@ console.log(cat1.name);*/
     }
 }
 
-let cat = new Cat('Sunny', 'Anna', 789456123, 'british shorthair');
-console.log(cat.name);
-console.log(cat.breed);*/
+function saveInfo() {
+    
+}
 
 
 
